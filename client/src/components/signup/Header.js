51cda8c7205de2015dfcb49726b641a36ccd4599
iriahbee <solagnelake@gmail.com>
2022-05-05 20:21:53 +0100

    Working Form

---
+++ client/src/components/signup/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Header = () => (
+  <div>
+    <h1>Multi Step Registration</h1>
+  </div>
+);
+
+export default Header;
--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,77 +1,37 @@
-import {React, useState, useEffect } from 'react';
+import { React, useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
-import UserDetails from './UserDetails';
-import PersonalDetails from './PersonalDetails';
-import ProfileDescription from './ProfileDescription';
-import ProfilePhotos from './ProfilePhotos';
-// import Logo from './Logo';
-
+import UserDetails from "./UserDetails";
+// import PersonalDetails from './PersonalDetails';
+// import ProfileDescription from './ProfileDescription';
+// import ProfilePhotos from './ProfilePhotos';
+import Header from "./Header";
 
 const StyledContainer = styled.div`
-  display: flex;
-  width: 100%;
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-  height: 300px;
+ display: flex;
+ padding: 4em;
+ background: papayawhip;
+ flex-direction: column; 
+ justify-content: center;
+ align-items: center;
+ position: relative;
+`;
+const FormContainer = styled.div`
+  display: block;
+  background: greenyellow;
 `;
 
-const WAIT_INTERVAL = 750;
-let progressTimer = null;
-
-function SignUpForm() {
-    const [step, setStep] = useState(0);
-
-    const [formData, setFormData] = useState({
-      email: "",
-    });
-
-    const [completedSteps, setCompletedSteps] = useState({
-      UserDetails: false,
-      PersonalDetails: false,
-      ProfileDescription: false,
-      ProfilePhotos: false,
-    });
-  
-    const handleChange = (evt) => {
-      const { id, value } = evt.target;
-      setFormData((prevState) => ({
-        ...prevState,
-        [id]: value,
-      }));
-      console.log(formData.email);
-    };
-
-    useEffect(() => {
-      if (formData.email.length > 0) {
-        setCompletedSteps((prevState) => ({
-          ...prevState,
-          email: true,
-        }));
-      }
-    }, [formData]);
-  
-    useEffect(() => {
-      if (completedSteps.step0 && !completedSteps.step1) {
-        clearTimeout(progressTimer);
-        progressTimer = setTimeout(() => {
-          return setStep(1);
-        }, WAIT_INTERVAL);
-      }
-    });
-  
-    return (
-      <>
-        <StyledContainer>
-        {step === 0 && <UserDetails onClick={handleChange} />} 
-        {step === 1 && <PersonalDetails />}
-        {step === 2 && <ProfileDescription />}
-        {step === 3 && <ProfilePhotos />}
-      </StyledContainer>
-      </>
-    );
-  };
-
+const SignUpForm = () => (
+  <BrowserRouter>
+    <StyledContainer>
+    <Header />
+    <FormContainer>
+      <Switch>
+        <Route path="/" component={UserDetails} />
+      </Switch>
+      </FormContainer>
+    </StyledContainer>
+  </BrowserRouter>
+);
 
 export default SignUpForm;
-
